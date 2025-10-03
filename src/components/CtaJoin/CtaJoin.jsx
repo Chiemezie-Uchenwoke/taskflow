11735565335977825afc9f67379a47419c32c242
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-10-03 14:52:56 +0100

    Complete CTA section

--- src/components/CtaJoin/CtaJoin.jsx
+++ src/components/CtaJoin/CtaJoin.jsx
@@ -10,17 +10,17 @@ const CtaJoin = () => {
                     Join thousands of teams who trust TaskFlow to manage their projects efficiently.
                 </p>
 
-                <div className="flex flex-col gap-2 w-full sm:flex-row sm:justify-center">
-                    <button className="capitalize bg-white-shade text-pri-blue py-2.5 lg:py-4 my-3 px-8 cursor-pointer border-2 border-white-shade hover:brightness-95 duration-200 rounded-lg self-stretch font-semibold lg:text-lg shadow-xl">
+                <div className="flex flex-col gap-4 lg:gap-8 w-full sm:flex-row sm:justify-center py-3">
+                    <button className="capitalize bg-white-shade text-pri-blue py-3.5 lg:py-4 px-8 cursor-pointer border-2 border-white-shade hover:brightness-95 duration-200 rounded-lg self-stretch font-semibold lg:text-lg shadow-xl">
                         start free trial
                     </button>
 
-                    <button className="capitalize text-white-shade py-2.5 lg:py-4 my-3 px-8 cursor-pointer border-2 border-white-shade hover:bg-white-shade hover:text-pri-blue duration-200 rounded-lg self-stretch font-semibold lg:text-lg">
+                    <button className="capitalize text-white-shade py-3.5 lg:py-4 px-8 cursor-pointer border-2 border-white-shade hover:bg-white-shade hover:text-pri-blue duration-200 rounded-lg self-stretch font-semibold lg:text-lg">
                         Schedule Demo
                     </button>
                 </div>
 
-                <p className="text-blue-100 text-sm relative -top-1.5">
+                <p className="text-blue-100 text-sm relative -top-1.5 text-center">
                     No credit card required • 14-day free trial • Cancel anytime
                 </p>
             </div>
