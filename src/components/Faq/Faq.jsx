ee784f85a70355d23dbfbc3f1c0a5be81144690f
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-10-03 13:51:36 +0100

    Complete FAQ section

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,16 +1,36 @@
 import SectionIntro from "../SectionIntro/SectionIntro";
+import faqData from "../../data/faqData";
+import FaqCard from "./FaqCard";
+import { useState } from "react";
 
 const Faq = () => {
+    const [openFaqId, setOpenFaqId] = useState(null);
+
+    const toggleFaq = (id) => {
+        setOpenFaqId(prevId => (prevId === id ? null : id));
+    };
+
     return (
         <section className="mt-18 bg-pri-blue/2 py-16" id="faq">
-            <div className="container">
+            <div className="container flex flex-col items-center gap-10 lg:gap-12">
                 <SectionIntro 
                     title="Frequently Asked Questions"
                     subtitle="Everything you need to know about TaskFlow"
                 />
 
-                <div>
-                    
+                <div className="flex flex-col gap-4 w-full items-center">
+                    {
+                        faqData.map(f => {
+                            return (
+                                <FaqCard 
+                                    key={f.id}
+                                    {...f}
+                                    isOpen={openFaqId === f.id}
+                                    onClick={() => toggleFaq(f.id)}
+                                />
+                            );
+                        })
+                    }
                 </div>
             </div>
         </section>
---
+++ src/components/Faq/FaqCard.jsx
@@ -0,0 +1,29 @@
+import { IoIosArrowDown } from "react-icons/io";
+
+const FaqCard = ({question, answer, isOpen, onClick}) => {
+    return (
+        <div 
+            className="w-full max-w-[45rem] border border-black/10 py-6 px-4 lg:px-6 bg-white-shade rounded-2xl cursor-pointer flex flex-col gap-2"
+            onClick={onClick}
+        >
+            <div className="flex justify-between items-center w-full">
+                <h3 className="font-semibold select-none">
+                    {question}
+                </h3>
+
+                <span className={`text-xl select-none transform transition-transform duration-300 ${isOpen ? "rotate-180" : ""}`}>
+                    <IoIosArrowDown />
+                </span>
+            </div>
+
+            {
+                isOpen && 
+                <div className="select-none text-dark-bg/70">
+                    {answer}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default FaqCard;
